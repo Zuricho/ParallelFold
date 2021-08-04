df54563dd9dc516207a087140b736f6f309d7a08
Zuricho <zbztzhz@163.com>
2021-08-04 14:58:16 +0800

    changed run_feature.sh

--- run_feature.sh
+++ run_feature.sh
@@ -85,8 +85,7 @@ if [[ "$preset" != "full_dbs" && "$preset" != "casp14" ]] ; then
 fi
 
 # This bash script looks for the run_alphafold.py script in its current working directory, if it does not exist then exits
-current_working_dir=$(pwd)
-alphafold_script="$current_working_dir/run_feature.py"
+alphafold_script="$(readlink -f $(dirname $0))/run_alphafold.py"  
 
 if [ ! -f "$alphafold_script" ]; then
     echo "Alphafold python script $alphafold_script does not exist."
